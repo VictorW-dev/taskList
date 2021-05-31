7931853c84d6bbe2816f3ea03938b85715b10880
VictorW-dev <victorwinicius.bs@gmail.com>
2021-05-31 01:44:20 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import TaskList from './src/components/TaskList';
+import * as Animatable from 'react-native-animatable';
+
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
 export default function App() {
+  const [task, setTask] = useState([
+    {key: 1, task: 'Comprar pão'},
+    {key: 2, task: 'Comprar Memória RAM'},
+    {key: 3, task: 'Comprar Notebook'},
+    {key: 4, task: 'Estudar Lógica'},
+    {key: 5, task: 'Comprar refrigerante'},
+  ])
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor = "#171d31" barStyle = "light-content" />
+
+      <View style={styles.content}>
+      <Text style={styles.title}>Minhas Tarefas</Text>
+      </View>
+
+      <FlatList
+      marginHorizontal = {10}
+      showsHorizontalScrollIndicator={false}
+      data={task}
+      keyExtractor={(item) => String(item.key) }
+      renderItem={({item}) => <TaskList data ={item} />}
+      />
+
+      <Modal animationType="slide" transparent={false} visible={open}> 
+        <SafeAreaView>
+          <Text>
+            Modal 123
+          </Text>
+        </SafeAreaView>
+      </Modal>
+
+      <AnimatedBtn style={styles.fab}
+      useNativeDriver
+      animation="bounceInUp"
+      duration={1500}
+      onPress={ () => setOpen(true) }
+      >
+        <Ionicons name = "ios-add" size={35} color="#FFF"/>
+      </AnimatedBtn>
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#171d31'
+  },
+  title: {
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: 'center',
+    color:'#FFF'
+  },
+  fab: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    backgroundColor: '#0094FF',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset:{
+      width: 1,
+      height: 3,
+    }
   },
 });
